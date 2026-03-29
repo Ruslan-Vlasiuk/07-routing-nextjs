f2f62817a7cfec821348afaebe6b25e6f7f3c4ea
Ruslan Vlasiuk <ruslan.vlasiuk.0028@gmail.com>
2026-03-29 17:56:01 +0000

    fix: move @modal to app level, fix structure per requirements

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -4,20 +4,17 @@ import css from "./LayoutNotes.module.css";
 interface FilterLayoutProps {
   children: ReactNode;
   sidebar: ReactNode;
-  modal: ReactNode;
 }
 
 export default function FilterLayout({
   children,
   sidebar,
-  modal,
 }: FilterLayoutProps) {
   return (
     <div className={css.container}>
       <div className={css.sidebar}>{sidebar}</div>
       <div className={css.notesWrapper}>
         {children}
-        {modal}
       </div>
     </div>
   );
